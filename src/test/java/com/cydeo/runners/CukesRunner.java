7b25b930ac40df5f1f8da0f9b1382373de8aa03a
joskyday <joskyday@gmail.com>
2023-04-30 08:22:14 -0400

    3rd commit

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,31 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions"
+)
+public class CukesRunner {}
+
+
+
+
+
+
+
+
+
+//plugin = {
+//                "html:target/cucumber-reports.html",
+//                "rerun:target/rerun.txt",
+//                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+//        },
+//        features = "src/test/resources/features",
+//        glue = "com/cydeo/step_definitions",
+//        dryRun = false,
+//        tags = " @wip",
+//        publish = true //generating a report with public link
